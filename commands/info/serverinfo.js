a06ef73c96611b71516179b674b83a58d4d7d88e
David / entety <34923485+Entety101@users.noreply.github.com>
2020-06-25 09:51:53 -0500

    Add files via upload

---
+++ commands/info/serverinfo.js
@@ -0,0 +1,29 @@
+
+const { RichEmbed } = require("discord.js");
+
+
+module.exports = {
+
+    name: "serverinfo",
+
+    aliases: ["sinfo", "info"],
+
+    description: "Returns Bot information",
+
+    run: (client, message, args) => {
+
+        const embed = new RichEmbed()
+
+    .setDescription("Server Information")
+    .setColor("#2003fc")
+    .setThumbnail(message.guild.iconURL)
+    .addField("Server Name", message.guild.name)
+    .addField("Created On", message.guild.createdAt)
+    .addField("You Joined", message.member.joinedAt)
+    .addField("Total Members", message.guild.memberCount);
+
+        message.channel.send(embed);
+
+    }
+
+}
