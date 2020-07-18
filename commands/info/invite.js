54c13eea5d761553cb02f22ce1fee061cf6053f5
David / entety <dap9372@gmail.com>
2020-07-17 22:52:25 -0500

    Create invite.js

---
+++ commands/info/invite.js
@@ -0,0 +1,26 @@
+
+const { RichEmbed } = require("discord.js");
+
+
+module.exports = {
+
+    name: "invite",
+
+    aliases: ["botinvite", "invitebot"],
+
+    description: "Returns Bot information",
+
+    run: (client, message, args) => {
+
+        const embed = new RichEmbed()
+
+        .setColor("000000")
+        .setTitle("Click to invite Haste Bot!")
+        .setURL("https://discord.com/api/oauth2/authorize?client_id=733774256940515349&permissions=8&scope=bot")
+        .setTimestamp()
+
+        message.channel.send(embed);
+
+    }
+
+}
