9865169020dad336afbdf280587e52da7b7d3464
David / entety <dap9372@gmail.com>
2020-07-20 11:10:31 -0500

    updated invite to not be icky

--- commands/info/invite.js
+++ commands/info/invite.js
@@ -16,9 +16,9 @@ module.exports = {
 
         .setColor("#01000a")
         .setTitle("Click to invite Haste Bot!")
-        .setImage('https://cdn.discordapp.com/attachments/665508963068018688/665509427188596766/rainbowdivider_1.gif')
+        .setThumbnail('https://cdn.discordapp.com/avatars/733774256940515349/73143418d7c393e9e4f636e10af7d90b.png?size=2048')
         .setURL("https://discord.com/api/oauth2/authorize?client_id=733774256940515349&permissions=8&scope=bot")
-        .setTimestamp()
+        .setDescription('Bot made by Haste#1400')
 
         message.channel.send(embed);
 
