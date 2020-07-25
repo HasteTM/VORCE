46059e22f24f3e10b90c894aec5a410b3d5148e8
David / entety <dap9372@gmail.com>
2020-07-25 18:42:25 -0500

    ez

--- commands/info/support.js
+++ commands/info/support.js
@@ -14,7 +14,7 @@ module.exports = {
 // need to push new update with rgb theme
         .setColor('#102445')
         .setAuthor('Support', 'https://cdn.discordapp.com/attachments/664927615034982410/734857644464013414/LEGEND_01.gif')
-        .setURL('https://discord.gg/Drps7he')
+        .setURL('https://discord.gg/YUvxRty')
         .setTitle('Join Server here')
         .setDescription('Are you confused about how to use the bot or want to ask a question? Join the Support Server, and we can help you out!')
 
