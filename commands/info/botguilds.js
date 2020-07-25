7b0f4ffcd6726ed393909964a7bb89bd5bd11af2
David / entety <dap9372@gmail.com>
2020-07-24 20:38:22 -0500

    update botguild.js
    ez

--- commands/info/botguilds.js
+++ commands/info/botguilds.js
@@ -12,7 +12,7 @@ module.exports = {
         const embed = new RichEmbed()
 
         .setAuthor('Guilds', 'https://cdn.discordapp.com/attachments/664927615034982410/734857644464013414/LEGEND_01.gif')
-        .setDescription(`**Haste Bot is In __${client.guilds.size}__ servers!**`)
+        .setDescription(`**${client.user.username} Bot is currently in __${client.guilds.size}__ servers!**`)
         message.channel.send(embed);
 
     }
