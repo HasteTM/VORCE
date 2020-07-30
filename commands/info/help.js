b9658bbb7ecf7e322305404a209ea0e4b0466291
David / entety <dap9372@gmail.com>
2020-07-30 14:13:50 -0500

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -19,7 +19,7 @@ module.exports = {
         .addField('🔐 Moderation', '`say, purge, ban, kick, unban,`')
         .addField('✅ Bot', '`guilds, status, invite, support, eval, botstaff`')
         .addField('❗️ Info', '`avatar, serverinfo, userinfo`')
-        .addField('🎉 Fun', '`meme, rps, love, fortnite store`')
+        .addField('🎉 Fun', '`meme, rps, love,`')
         .addField('🎵 Music', '`Coming Soon!!`')
         .setImage('https://cdn.discordapp.com/attachments/665508963068018688/665509427188596766/rainbowdivider_1.gif')
 
