2257b75f1742b625df059bada404ac96361c9645
David / entety <dap9372@gmail.com>
2020-08-02 14:41:04 -0500

    Added blaze pogg

--- commands/bot/botstaff.js
+++ commands/bot/botstaff.js
@@ -12,7 +12,7 @@ module.exports = {
         const embed = new RichEmbed()
         .setColor('#830000')
         .setAuthor('Botstaff', 'https://cdn.discordapp.com/attachments/664927615034982410/734857644464013414/LEGEND_01.gif')
-        .addField('Developers',' <@651515978231971900>,  <@319516807297892371>,  <@709151479918755890>')
+        .addField('Developers',' <@651515978231971900>,  <@319516807297892371>,  <@709151479918755890>,  <@709523716333109369>')
         .addField('Bot Admins', '<@403634335736922132>,  <@698306752508198972>')
         message.channel.send(embed);
 
