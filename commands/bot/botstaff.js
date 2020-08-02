f4c76a7d10740cb67ec212708f685fb83a697ad1
David / entety <dap9372@gmail.com>
2020-08-02 15:12:13 -0500

    Update botstaff.js

--- commands/bot/botstaff.js
+++ commands/bot/botstaff.js
@@ -13,7 +13,7 @@ module.exports = {
         .setColor('#830000')
         .setAuthor('Botstaff', 'https://cdn.discordapp.com/attachments/664927615034982410/734857644464013414/LEGEND_01.gif')
         .addField('Developers',' <@651515978231971900>,  <@319516807297892371>,  <@709151479918755890>,  <@709523716333109369>')
-        .addField('Bot Admins', '<@403634335736922132>,  <@698306752508198972>')
+        .addField('Bot Admins', '<@403634335736922132>,  <@698306752508198972>,  <@642462432643514368>')
         message.channel.send(embed);
 
     }
