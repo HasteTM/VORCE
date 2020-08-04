f079191d53b6df7ee3b1f077d94ed5a6cac9e4ff
David / entety <dap9372@gmail.com>
2020-08-04 13:55:17 -0500

    UPDATE!!!!!!!!!!

--- commands/info/help.js
+++ commands/info/help.js
@@ -26,7 +26,7 @@ module.exports = {
         .addField('<:speak:740255369737273454> | Fun', '`meme` | `rps` | `love`')
         .addField('<:music:740255395402219591> | Music', '`play` | `pause` | `skip` | `stop` | `volume` | `queue` | `remove` | `np`')
         .addField('<:bilgi:740256839379189770> | Utility','`poll`')
-        .addField('<:tickee:740255341408682015> | Badge Documentation','`staff` | `partner` | `bughunter` | `verifiedbotdeveloper` | `hypesquadevent` | `earlysupporter`')
+        .addField('<:tickee:740255341408682015> | Badge Documentation','`staff` | `partner` | `bughunter` | `verifiedbotdeveloper` | `hypesquadevents` | `earlysupporter`')
         .setImage('https://media.discordapp.net/attachments/740250432387022859/740257331891142746/A7jrU3CyC7EaAAAAAElFTkSuQmCC.png')
 
         message.author.send(dms)
