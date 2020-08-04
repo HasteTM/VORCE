b9404c8b199ef02f7db62b95085326528f971399
David / entety <dap9372@gmail.com>
2020-08-04 17:24:35 -0500

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -22,10 +22,10 @@ module.exports = {
         .setColor('#7289da')
         .addField('<:work:740255311264350268> | Moderation', '`say` | `purge` | `ban` | `kick` | `unban`')
         .addField('<:bots:740255859761741836> | Bot', '`guild` | `status` | `invite` | `support` | `eval` | `botinfo`')
-        .addField('<:bilgi:740317093018468483> |  Info', '`avatar` | `serverinfo` | `userinfo` | `query` | `ping`')
+        .addField('<:bilgi:740317093018468483> |  Info', '`avatar` | `serverinfo` | `userinfo` | `ping`')
         .addField('<:speak:740255369737273454> | Fun', '`meme` | `rps` | `love`')
         .addField('<:music:740255395402219591> | Music', '`play` | `pause` | `skip` | `stop` | `volume` | `queue` | `remove` | `np`')
-        .addField('<:hum:740255276099305472> | Utility','`poll`')
+        .addField('<:hum:740255276099305472> | Utility','`poll` | `query`')
         .addField('<:tickee:740255341408682015> | Badge Documentation','`staff` | `partner` | `bughunter` | `vbdeveloper` | `hypesquadevents` | `earlysupporter`')
         .setImage('https://media.discordapp.net/attachments/740250432387022859/740257331891142746/A7jrU3CyC7EaAAAAAElFTkSuQmCC.png')
 
