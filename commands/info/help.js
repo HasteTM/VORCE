5dbfffb86d0960139ef0e7a67c2af87bb22c7859
David / entety <dap9372@gmail.com>
2020-08-04 09:33:26 -0500

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -23,12 +23,12 @@ module.exports = {
 
         .setColor('#8e2430')
         .setAuthor('Help', 'https://cdn.discordapp.com/attachments/664927615034982410/734857644464013414/LEGEND_01.gif')
-        .addField('🔐 | Moderation', '`say, purge, ban, kick, unban,`')
-        .addField('✅  | Bot', '`guilds, status, invite, support, eval, botinfo`')
-        .addField('❗️ |  Info', '`avatar, serverinfo, userinfo, query, ping`')
-        .addField('🎉 | Fun', '`meme, rps, love`')
-        .addField('🎵 | Music', '`play, pause, skip, stop, volume, queue, remove, np`')
-        .addField('🔧 | Utility','`poll`')
+        .addField('🔐 | Moderation', 'say, purge, ban, kick, unban,')
+        .addField('✅  | Bot', 'guilds, status, invite, support, eval, botinfo')
+        .addField('❗️ |  Info', 'avatar, serverinfo, userinfo, query, ping')
+        .addField('🎉 | Fun', 'meme, rps, love')
+        .addField('🎵 | Music', 'play, pause, skip, stop, volume, queue, remove, np')
+        .addField('🔧 | Utility','poll')
         .setImage('https://cdn.discordapp.com/attachments/665508963068018688/665509427188596766/rainbowdivider_1.gif')
 
 
