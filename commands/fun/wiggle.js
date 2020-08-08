28eaca11b3a222d0ad8a667272e3765ea11f2c4f
David / entety <dap9372@gmail.com>
2020-08-07 23:11:40 -0500

    Create wiggle.js

---
+++ commands/fun/wiggle.js
@@ -0,0 +1,68 @@
+
+const { RichEmbed } = require("discord.js");
+
+
+module.exports = {
+
+    name: "wiggle",
+
+    description: "wiggle",
+
+    run: (client, message, args) => {
+
+        if (!message.member.hasPermission('ADMINISTRATOR')) {
+            return message.channel.send('No wiggle fo yo')
+            .then(m => m.delete(5000));
+          }
+
+
+message.channel.send(`
+Wiggle
+Wiggle
+ Wiggle
+  Wiggle
+   Wiggle
+    Wiggle
+     Wiggle
+      Wiggle
+       Wiggle
+        Wiggle
+        Wiggle
+        Wiggle
+       Wiggle
+      Wiggle
+     Wiggle
+    Wiggle
+   Wiggle
+  Wiggle
+ Wiggle
+Wiggle
+Wiggle
+Wiggle
+Wiggle
+Wiggle
+ Wiggle
+  Wiggle
+   Wiggle
+    Wiggle
+     Wiggle
+      Wiggle
+       Wiggle
+        Wiggle
+        Wiggle
+        Wiggle
+       Wiggle
+      Wiggle
+     Wiggle
+    Wiggle
+   Wiggle
+  Wiggle
+ Wiggle
+Wiggle
+Wiggle
+
+`)
+
+    }
+
+}
