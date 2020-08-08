87c42dd4ca84962d26557ca1ba33673fd2012dfc
David / entety <dap9372@gmail.com>
2020-08-08 18:30:33 -0500

    added an announce command

--- commands/info/help.js
+++ commands/info/help.js
@@ -27,7 +27,7 @@ module.exports = {
         .addField('<:bilgi:740317093018468483> |  Info', '`avatar` | `serverinfo` | `userinfo` | `ping` | `help`')
         .addField('<:speak:740255369737273454> | Fun', '`meme` | `rps` | `love` | `gaymeter`')
         .addField('<:music:740255395402219591> | Music', '`play` | `pause` | `skip` | `stop` | `volume` | `queue` | `remove` | `np`')
-        .addField('<:hum:740255276099305472> | Utility','`poll` | `lockdown` | `role`')
+        .addField('<:hum:740255276099305472> | Utility','`poll` | `lockdown` | `role` | `announce`')
         .addField('<:tickee:740255341408682015> | Badge Documentation','`staff` | `partner` | `bughunter` | `vbdeveloper` | `hypesquadevents` | `earlysupporter`')
         .setImage('https://media.discordapp.net/attachments/740250432387022859/740257331891142746/A7jrU3CyC7EaAAAAAElFTkSuQmCC.png')
 
---
+++ commands/utility/announce.js
@@ -0,0 +1,46 @@
+const { RichEmbed, DiscordAPIError } = require("discord.js");
+
+module.exports = {
+    name: "announce",
+    description: "Says your input via the bot",
+    usage: "<input>",
+    run: (client, message, args) => {
+        message.delete();
+
+        const announce1 = new RichEmbed()
+        .setColor('#7289da')
+        .setDescription(`${args.join(" ")}`)
+        .setTimestamp()
+        .setFooter(message.author.username, message.author.displayAvatarURL)
+
+        const say1 = new RichEmbed()
+        .setColor('#7289da')
+        .setAuthor("You don't have permission to use this command.")
+        .setTimestamp()
+        .setFooter(message.author.username, message.author.displayAvatarURL)
+
+        const say2 = new RichEmbed()
+        .setColor('#7289da')
+        .setAuthor("Please provide a message for me to announce!")
+        .setTimestamp()
+        .setFooter(message.author.username, message.author.displayAvatarURL)
+
+        if (!message.member.hasPermission("ADMINISTRATOR"))
+            return message.channel.send(say1).then(m => m.delete(5000));
+
+        if (args.length < 0)
+            return message.channel.send(say2).then(m => m.delete(5000));
+
+        const roleColor = message.guild.me.highestRole.hexColor;
+
+        if (args[0].toLowerCase() === "embed") {
+            const embed = new RichEmbed()
+                .setDescription(args.slice(1).join(" "))
+                .setColor(roleColor === "#000000" ? "#ffffff" : roleColor);
+
+            message.channel.send(embed);
+        } else {
+            message.channel.send(announce1);
+        }
+    }
+}
