c7964926f89baf5943d7003a25816053c5583e46
David / entety <dap9372@gmail.com>
2020-08-07 22:17:42 -0500

    Update poll.js

--- commands/utility/poll.js
+++ commands/utility/poll.js
@@ -16,7 +16,7 @@ module.exports = {
             
             const poll1 = new RichEmbed()
             .setColor('#7289da')
-            .setTitle("If you want to create a poll, please do v!poll (message) to start a poll.")
+            .setDescription("If you want to create a poll, please do v!poll (message) to start a poll.")
             .setTimestamp()
             .setFooter(`${message.author.username}`, message.author.avatarURL)
 
