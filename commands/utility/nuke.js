da7b8bc6f01eab2f67ef66b610db920d15f1e148
David / entety <dap9372@gmail.com>
2020-08-09 12:45:52 -0500

    Update nuke.js

--- commands/utility/nuke.js
+++ commands/utility/nuke.js
@@ -1,7 +1,7 @@
 const { RichEmbed } = require('discord.js');
 
 module.exports = {
-  name: 'nuke',
+  name: 'tnuke',
   description: 'Nukes the current channel.',
   run: (client, message) => {
 
@@ -13,11 +13,9 @@ module.exports = {
 
     const authorLogo = 'https://media.discordapp.net/attachments/651589704772485131/740315645106978876/hum.png';
     const newChannelEmbed = new RichEmbed()
-      .setColor('#7289da')
-      .setAuthor('Nuke', authorLogo)
+      .setColor('#36393f')
+      .setAuthor(`${message.author.username} Successfully Nuked the Channel.`)
       .setImage('https://cdn.discordapp.com/attachments/651589704772485131/741823383955832883/SHKS.gif')
-      .setTimestamp()
-      .setFooter(message.author.username, message.author.displayAvatarURL)
 
     if (!(message.member.hasPermission('ADMINISTRATOR') || message.member.hasPermission('MANAGE_CHANNELS'))) {
         return message.channel.send(perms1)
