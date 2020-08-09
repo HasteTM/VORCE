3e26e1a7922d4ee4c07283cdc9c7324838a21537
David / entety <dap9372@gmail.com>
2020-08-09 17:38:20 -0500

    added a snipe command

---
+++ commands/utility/snipe.js
@@ -0,0 +1,45 @@
+const { RichEmbed } = require('discord.js');
+const snipeStorage = require('../../storage/snipe.json');
+
+module.exports = {
+  name: 'snipe',
+  description: 'Displays the last deleted message.',
+  run: (client, message) => {
+    const { channel } = message;
+    const authorLogo = 'https://media.discordapp.net/attachments/651589704772485131/740315645106978876/hum.png';
+
+    const noPermsEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor('Snipe', authorLogo)
+      .setDescription('⛔ You don\'t have enough permissions to use this command.')
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    if (!(message.member.hasPermission('ADMINISTRATOR'))) {
+      return channel.send(noPermsEmbed);
+    }
+
+    const snipedEmbed = (msg, author, timeStamp) => new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor('Snipe', authorLogo)
+      .setDescription(`Last message:\n**Deleted at:** ${timeStamp}\n**Author:** ${author}\n**Content:** ${msg}`)
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const nothingToSnipeEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor('Snipe', authorLogo)
+      .setDescription('⛔ There is nothing to snipe in this channel.')
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const channelID = message.channel.id;
+
+    if (snipeStorage[channelID] && snipeStorage[channelID].lastMsgCont.length > 0) {
+      const { lastMsgCont, lastMsgAuth, delAt } = snipeStorage[channelID];
+      channel.send(snipedEmbed(lastMsgCont, lastMsgAuth, delAt));
+    } else {
+      channel.send(nothingToSnipeEmbed);
+    }
+  },
+};
--- index.js
+++ index.js
@@ -1,86 +1,89 @@
-const Discord = require("discord.js");
-
-const { Client, Collection} = require("discord.js");
-
-const MusicBot = require("vorce-music-module");
-
-const { config } = require("dotenv");
-
+/* eslint-disable max-len */
+const Discord = require('discord.js');
+const MusicBot = require('vorce-music-module');
+const fs = require('fs');
+const { Client, Collection } = require('discord.js');
+const { config } = require('dotenv');
+const snipeStorage = require('./storage/snipe.json');
 
 const client = new Client({
-
-    disableEveryone: true
-
-})
+  disableEveryone: true,
+});
 
 client.commands = new Collection();
-
 client.aliases = new Collection();
 
 config({
-
-    path: __dirname + "/.env"
-
+  path: `${__dirname}/.env`,
 });
 
-["command"].forEach(handler => {
-
-    require(`./handlers/${handler}`)(client);
-
+['command'].forEach((handler) => {
+  require(`./handlers/${handler}`)(client);
 });
 
-client.on("ready", () => {
-
-    console.log(` ${client.user.username} is now appearing online, in ${client.guilds.size} servers!`);
-    client.user.setStatus('dnd');
-    const activities_list = [
+client.on('ready', () => {
+  console.log(`${client.user.username} is now appearing online, in ${client.guilds.size} servers!`);
+  client.user.setStatus('dnd');
+  const activitesList = [
     `${client.users.size} users`,
-    "for -help",
+    'for v!help',
     `${client.guilds.size} servers!`,
-    ]; // creates an arraylist containing phrases you want your bot to switch through.
+  ]; // creates an arraylist containing phrases you want your bot to switch through.
 
-    setInterval(() => {
-    const index = Math.floor(Math.random() * (activities_list.length - 1) + 1); // generates a random number between 1 and the length of the activities array list (in this case 5).
-    client.user.setActivity(`${activities_list[index]}`, { type: "WATCHING" });
-    }, 10000);
+  setInterval(() => {
+    // generates a random number between 1 and the length of the activities array (in this case 5).
+    const index = Math.floor(Math.random() * (activitesList.length - 1) + 1);
+    client.user.setActivity(`${activitesList[index]}`, { type: 'WATCHING' });
+  }, 10000);
 });
 
-client.on("message", async message => {
-  if (message.channel.type === "dm"){
-    let dmEmbed = new Discord.RichEmbed()
-    .setFooter(`${message.author.username}#${message.author.discriminator}: ${message.content}`, message.author.avatarURL)
+client.on('message', async (message) => {
+  if (message.channel.type === 'dm') {
+    const dmEmbed = new Discord.RichEmbed()
+      .setFooter(`${message.author.username}#${message.author.discriminator}: ${message.content}`, message.author.avatarURL);
     client.channels.get('740001399877795921').send(dmEmbed);
-};
-    const prefix = "-";
+  }
+  const prefix = '-';
 
-    if (message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
 
-    if (!message.guild) return;
+  if (message.author.bot || !message.guild || !message.content.startsWith(prefix) || cmd.length < 1) {
+    return;
+  }
 
-    if (!message.content.startsWith(prefix)) return;
+  let { member } = message;
+  if (!member) {
+    member = await message.guild.fetchMember(message);
+  }
 
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-
-    const cmd = args.shift().toLowerCase();
-
-    if (cmd.length === 0) return;
-
-    let command = client.commands.get(cmd);
-
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-    if (command)
-
-        command.run(client, message, args);
+  let command = client.commands.get(cmd);
+  if (!command) {
+    command = client.commands.get(client.aliases.get(cmd));
+  } else {
+    command.run(client, message, args);
+  }
 });
-//music module below do not change
+
+client.on('messageDelete', (message) => {
+  const timeStamp = new Date().toLocaleString();
+
+  if (message.channel.type !== 'text' || message.author.bot) {
+    return;
+  }
+
+  const snipeStorageFile = './storage/snipe.json';
+  snipeStorage[message.channel.id] = { lastMsgCont: `${message.content}`, lastMsgAuth: `${message.author.tag}`, delAt: `${timeStamp}` };
+
+  fs.writeFileSync(snipeStorageFile, JSON.stringify(snipeStorage, null, 4));
+});
+
+// music module below do not change
 const bot = new MusicBot({
-    token: `${process.env.token}`,
-    ytApiKey: `${process.env.YTapikey}`,
-    prefix: `-`,
-    game: ' '
+  token: `${process.env.token}`,
+  ytApiKey: `${process.env.YTapikey}`,
+  prefix: '-',
+  game: ' ',
 });
 
 bot.run(); // Run the music module
