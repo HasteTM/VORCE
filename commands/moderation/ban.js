8d5e8ca18c28d0f355cbc1938383bad13b85c028
David / entety <dap9372@gmail.com>
2020-08-13 20:48:37 -0500

    updated kick and ban

--- commands/moderation/ban.js
+++ commands/moderation/ban.js
@@ -1,169 +1,91 @@
-const { RichEmbed, DiscordAPIError } = require("discord.js");
-
-const { stripIndents } = require("common-tags");
-
-const { promptMessage } = require("../../functions.js");
+const { RichEmbed } = require('discord.js');
 
 module.exports = {
-
-    name: "ban",
-
-    category: "moderation",
-
-    description: "bans the member",
-
-    usage: "<id | mention>",
-
-    run: async (client, message, args) => {
-
-        let defaultcolor = `#000000`
-
-        const canceled = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription(`<a:crossmark1:742750308089856022> Ban has been canceled!`)
-
-
-        const ban1 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription(`<a:crossmark1:742750308089856022> Please provide a member to ban.`)
-
-
-        const ban2 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription('<a:crossmark1:742750308089856022> Please provide a reason to ban.')
-
-
-
-        const ban3 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> You don't have permission to use this command.")
-
-
-        const ban4 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> Bot does not have permission to ban members.")
-
-
-        const ban5 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> Couldn't find that member, try again")
-
-
-        const ban6 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> You can't ban yourself.")
-
-
-        const ban7 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> I can't ban that member due to role hierarchy.")
-
-
-        const logChannel = message.guild.channels.find(c => c.name === "logs034768734867") || message.channel;
-
-        if (message.deletable) message.delete();
-
-        if (!args[0]) {
-
-            return message.channel.send(ban1)
-
-
-        }
-
-        if (!args[1]) {
-
-            return message.channel.send(ban2)
-
-
-        }
-
-        if (!message.member.hasPermission("BAN_MEMBERS")) {
-
-            return message.channel.send(ban3)
-
-
-        
-
-        }
-
-        if (!message.guild.me.hasPermission("BAN_MEMBERS")) {
-
-            return message.channel.send(ban4)
-
-
-        }
-        const toBan = message.mentions.members.first() || message.guild.members.get(args[0]);
-
-        if (!toBan) {
-
-            return message.channel.send(ban5)
-
-
-        }
-
-        if (toBan.id === message.author.id) {
-
-            return message.channel.send(ban6)
-
-
-        }
-
-        if (!toBan.bannable) {
-
-            return message.channel.send(ban7)
-
-
-        }
-
-        const embed = new RichEmbed()
-
-            .setAuthor('Ban')
-
-            .setColor(defaultcolor)
-
-            .setDescription(stripIndents`** Banned member:** ${toBan}
-
-            ** Banned By:** ${message.member}
-
-            ** Reason:** ${args.slice(1).join(" ")}`);
-
-        const promptEmbed = new RichEmbed()
-
-            .setColor(defaultcolor)
-
-            .setAuthor(`This verification becomes invalid after 30 minutes.`)
-
-            .setDescription(`Do you want to ban ${toBan}?`)
-
-        await message.channel.send(promptEmbed).then(async msg => {
-
-            const emoji = await promptMessage(msg, message.author, 30000, ["✅", "❌"]);
-
-            if (emoji === "✅") {
-
-                msg.delete();
-
-                toBan.ban(args.slice(1).join(" "))
-
-                    .catch(err => {
-
-                        if (err) return message.channel.send(`Error Ban | ${err}`)
-
-                    });
-
-                logChannel.send(embed);
-
-            } else if (emoji === "❌") {
-
-                msg.delete();
-
-                message.channel.send(canceled)
-
-
-            }
-
-        });
-
+  name: 'ban',
+  category: 'moderation',
+  description: 'bans the member',
+  usage: '<id | mention>',
+  run: async (client, message, args) => {
+    const noUserEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor('⚠️ Please provide a person to ban.')
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const noPermsUserEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("❗ You don't have permission to use this command.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const noPermsBotEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor('❗ Bot does not have permission to ban members.')
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const couldntFindMemberEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("⚠️ Couldn't find that member.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const cantBanSelfEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("⚠️ You can't ban yourself.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const roleNotHighEnoughEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("⚠️ I can't ban that member due to role hierarchy.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const [user] = args[0];
+
+    if (!user) {
+      return message.channel.send(noUserEmbed)
+        .then((m) => m.delete(5000));
     }
 
+    if (!message.member.hasPermission('BAN_MEMBERS')) {
+      return message.channel.send(noPermsUserEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    if (!message.guild.me.hasPermission('BAN_MEMBERS')) {
+      return message.channel.send(noPermsBotEmbed)
+        .then((m) => m.delete(5000));
+    }
+    const toBan = message.mentions.members.first() || message.guild.members.get(user);
+    args.splice(0, 1);
+
+    if (!toBan) {
+      return message.channel.send(couldntFindMemberEmbed)
+
+        .then((m) => m.delete(5000));
+    }
+
+    if (toBan.id === message.author.id) {
+      return message.channel.send(cantBanSelfEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    if (!toBan.bannable) {
+      return message.channel.send(roleNotHighEnoughEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    const successBanEmbed = new RichEmbed()
+      .setAuthor('Ban', 'https://cdn.discordapp.com/attachments/651589704772485131/740315492023009431/work.png')
+      .setColor('#7289da')
+      .setDescription(`** Banned member:** ${toBan}
+            ** Banned by:** ${message.member}
+            ** Reason:** ${args.join(' ') || 'No reason provided.'}`);
+
+    toBan.ban(args.join(' ') || 'No reason provided.').then((v) => {
+      message.channel.send(successBanEmbed);
+    }).catch(console.log);
+  },
 };
--- commands/moderation/kick.js
+++ commands/moderation/kick.js
@@ -1,166 +1,92 @@
-const { RichEmbed } = require("discord.js");
-
-const { stripIndents } = require("common-tags");
-
-const { promptMessage } = require("../../functions.js");
+const { RichEmbed } = require('discord.js');
 
 module.exports = {
-
-    name: "kick",
-
-    category: "moderation",
-
-    description: "Kicks the member",
-
-    usage: "<id | mention>",
-
-    run: async (client, message, args) => {
-
-        let defaultcolor = `#000000`
-
-        const canceled = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription(`<a:crossmark1:742750308089856022> Kick has been canceled!`)
-
-
-        const kick1 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription(`<a:crossmark1:742750308089856022> Please provide a member to kick.`)
-
-
-        const kick2 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription('<a:crossmark1:742750308089856022> Please provide a reason to kick.')
-
-        
-        const kick3 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> You don't have permission to use this command.")
-
-        
-        const kick4 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> Bot does not have permission to kick members.")
-
-        
-        const kick5 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> Couldn't find that member, try again")
-
-        
-        const kick6 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> You can't kick yourself.")
-
-        
-        const kick7 = new RichEmbed()
-        .setColor(defaultcolor)
-        .setDescription("<a:crossmark1:742750308089856022> I can't kick that member due to role hierarchy.")
-
-        
-        const logChannel = message.guild.channels.find(c => c.name === "logs34978937458") || message.channel;
-
-        if (message.deletable) message.delete();
-
-        if (!args[0]) {
-
-            return message.channel.send(kick1)
-
-
-        }
-
-        if (!args[1]) {
-
-            return message.channel.send(kick2)
-
-
-        }
-
-        if (!message.member.hasPermission("KICK_MEMBERS")) {
-
-            return message.channel.send(kick3)
-
-
-        }
-
-        if (!message.guild.me.hasPermission("KICK_MEMBERS")) {
-
-            return message.channel.send(kick4)
-
-
-        }
-
-        const toKick = message.mentions.members.first() || message.guild.members.get(args[0]);
-
-        if (!toKick) {
-
-            return message.channel.send(kick5)
-
-        }
-
-        if (toKick.id === message.author.id) {
-
-            return message.channel.send(kick6)
-
-
-        }
-
-        if (!toKick.kickable) {
-
-            return message.channel.send(kick7)
-
-
-        }
-
-        const embed = new RichEmbed()
-
-            .setAuthor('Kick')
-
-            .setColor(defaultcolor)
-
-            .setDescription(stripIndents`** Kicked member:** ${toKick})
-
-            ** Kicked by:** ${message.member}
-
-            ** Reason:** ${args.slice(1).join(" ")}`);
-
-        const promptEmbed = new RichEmbed()
-
-            .setColor(defaultcolor)
-
-            .setAuthor(`This verification becomes invalid after 30 minutes.`)
-
-            .setDescription(`Do you want to kick ${toKick}?`)
-
-        await message.channel.send(promptEmbed).then(async msg => {
-
-            const emoji = await promptMessage(msg, message.author, 30, ["✅", "❌"]);
-
-            if (emoji === "✅") {
-
-                msg.delete();
-
-                toKick.kick(args.slice(1).join(" "))
-
-                    .catch(err => {
-
-                        if (err) return message.channel.send(`Error Kick |  ${err}`)
-
-                    });
-
-                logChannel.send(embed);
-
-            } else if (emoji === "❌") {
-
-                msg.delete();
-
-                message.channel.send(canceled)
-
-
-            }
-
-        });
-
+  name: 'kick',
+  category: 'moderation',
+  description: 'Kicks the member',
+  usage: '<id | mention>',
+
+  run: async (client, message, args) => {
+    const noUserEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor('⚠️ Please provide a person to kick.')
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const noPermsUserEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("❗ You don't have permission to use this command.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const noPermsBotEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor('❗ Bot does not have permission to kick members.')
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const couldntFindMemberEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("⚠️ Couldn't find that member.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const cantKickSelfEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("⚠️ You can't kick yourself.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const roleNotHighEnoughEmbed = new RichEmbed()
+      .setColor('#7289da')
+      .setAuthor("⚠️ I can't kick that member due to role hierarchy.")
+      .setTimestamp()
+      .setFooter(message.author.username, message.author.displayAvatarURL);
+
+    const [user] = args;
+
+    if (!user) {
+      return message.channel.send(noUserEmbed)
+        .then((m) => m.delete(5000));
     }
 
+    if (!message.member.hasPermission('KICK_MEMBERS')) {
+      return message.channel.send(noPermsUserEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    if (!message.guild.me.hasPermission('KICK_MEMBERS')) {
+      return message.channel.send(noPermsBotEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    const toKick = message.mentions.members.first() || message.guild.members.get(user);
+    args.splice(0, 1);
+
+    if (!toKick) {
+      return message.channel.send(couldntFindMemberEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    if (toKick.id === message.author.id) {
+      return message.channel.send(cantKickSelfEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    if (!toKick.kickable) {
+      return message.channel.send(roleNotHighEnoughEmbed)
+        .then((m) => m.delete(5000));
+    }
+
+    const successKickEmbed = new RichEmbed()
+      .setAuthor('Kick', 'https://cdn.discordapp.com/attachments/651589704772485131/740315492023009431/work.png')
+      .setColor('#7289da')
+      .setDescription(`** Kicked member:** ${toKick}
+            ** Kicked by:** ${message.member}
+            ** Reason:** ${args.join(' ') || 'No reason provided.'}`);
+
+    toKick.kick(args.join(' ') || 'No reason provided.').then((v) => {
+      message.channel.send(successKickEmbed);
+    }).catch(console.log);
+  },
 };
