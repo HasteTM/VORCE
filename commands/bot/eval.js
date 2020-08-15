9647a22bf81729dd4a4c1b48a3bdc6e9dc0e8805
CadetMercury615 <60723938+CadetMercury615@users.noreply.github.com>
2020-08-14 21:16:35 -0400

    Add epic eval command

---
+++ commands/bot/eval.js
@@ -0,0 +1,56 @@
+const { RichEmbed } = require("discord.js");
+const beautify = require("beautify"); 
+
+module.exports = {
+name: "eval",
+aliases: ["ev"],
+description: "Evaluates the code you put in", usage: "<code to eval>",
+run: async (client, message, args) => {
+
+     let defaultcolor = `#000000`
+     let evalDudes = [
+     "651515978231971900",
+     "522895569039917066",
+     "403634335736922132",
+     "522080989057515533",
+     "319516807297892371",
+     "709151479918755890", 
+     ]
+     if (!evalDudes.includes(msg.author.id)) {
+        return message.channel.send("<a:crossmark1:742750308089856022> You're not a Vorce Admin or a Developer!") 
+        .then(m => m.delete(5000));
+    }
+
+    if (!args[0]) {
+         message.channel.send("<a:crossmark1:742750308089856022> You need to evaluate something...")
+              .then(m => m.delete(5000));
+
+    }
+
+    try {
+    if (args.join(" ").toLowerCase().includes("token")) {
+      return;
+    }
+
+    const toEval = args.join (" ");
+    const evaluated = eval(toEval);
+
+    let embed = new RichEmbed()
+    .setColor(defaultcolor)
+    .setAuthor("Eval")
+    .addField("To evaluate:", `\`\`\`js\n${beautify(args.join(" "), { format: "js" })}\n\`\`\``)
+    .addField("Evaluated:", evaluated);
+
+    message.channel.send(embed);  
+
+} catch (e) {
+
+  let embed = new RichEmbed()
+  .setColor("#c4121b")
+  .setTitle('\:x: Error!')
+  .setDescription(e);
+
+  message.channel.send(embed);
+       }
+    }
+}
