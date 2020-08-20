bec7d1eb6ea16c84f9a014963efe0d9650399a15
David / entety <dap9372@gmail.com>
2020-08-20 18:38:06 -0500

    Update urban.js

--- commands/fun/urban.js
+++ commands/fun/urban.js
@@ -13,24 +13,32 @@ module.exports = {
 
         let rederrorcolor = `#fba6ff`
 
+        const msg1 = new RichEmbed()
+        .setColor(rederrorcolor)
+        .setDescription('<a:crossmark1:742750308089856022> Please enter a word.')
+
+        const msg2 = new RichEmbed()
+        .setColor(rederrorcolor)
+        .setDescription('<a:crossmark1:742750308089856022> No such word exists!')
+
         if (args.length < 1) {
-            return message.channel.send('Please enter a word');
+            return message.channel.send(msg1);
         }
         let word = args.join(' ');
         console.log(word);
 
         urban(word).first(json => {
             if (!json) {
-                return message.channel.send('No such word exists!');
+                return message.channel.send(msg2);
             }
             console.log(json);
             const embed1 = new RichEmbed()
+                .setAuthor('Urban')
                 .setTitle(json.word)
                 .setColor(defaultcolor)
                 .setDescription(json.definition)
                 .addField('Upvotes', json.thumbs_up, true)
                 .addField('Downvotes', json.thumb_down, true)
-                .setTimestamp(new Date())
                 .setFooter(`Written by ${json.author}`);
 
             message.channel.send(embed1);
